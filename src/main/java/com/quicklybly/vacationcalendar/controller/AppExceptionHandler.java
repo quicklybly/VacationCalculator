5603c5c61092ef1f013ecb49c29dcb0939431aa9
quicklybly <quickly.tawer@gmail.com>
2023-05-07 17:30:17 +0300

    add validation exception handler

---
+++ src/main/java/com/quicklybly/vacationcalendar/controller/AppExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.quicklybly.vacationcalendar.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class AppExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException e) {
+        var fieldErrors = e.getBindingResult().getFieldErrors();
+        return fieldErrors.stream()
+                .collect(Collectors.toMap(FieldError::getField, error -> {
+                    var message = Optional.ofNullable(error.getDefaultMessage());
+                    return message.orElse("");
+                }));
+    }
+}
