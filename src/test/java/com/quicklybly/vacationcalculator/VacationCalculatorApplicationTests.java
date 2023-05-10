f37f8f59d460010db8cb0c949998c20e5980d91c
quicklybly <quickly.tawer@gmail.com>
2023-05-10 20:49:04 +0300

    add docker and docker compose

--- src/test/java/com/quicklybly/vacationcalculator/VacationCalculatorApplicationTests.java
+++ src/test/java/com/quicklybly/vacationcalculator/VacationCalculatorApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VacationCalculatorApplicationTests {
-
+    //TODO - add units
     @Test
     void contextLoads() {
     }
